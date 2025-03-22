63599f484b293d621840ad0d4ecd5044049fcdfa
Админ <matroskin8@i.ua>
2025-03-22 12:32:31 +0200

    lesson 13

--- app/Livewire/User/UserList.php
+++ app/Livewire/User/UserList.php
@@ -4,6 +4,7 @@ namespace App\Livewire\User;
 
 use App\Models\User;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
 use Livewire\WithPagination;
@@ -12,6 +13,24 @@ class UserList extends Component
 {
 
     use WithPagination;
+
+    #[Url]
+    public int $limit = 10;
+    public array $limitList = [10, 25, 50, 100];
+
+    public function mount()
+    {
+        if (!in_array($this->limit, $this->limitList)) {
+            $this->redirectRoute('home');
+        }
+    }
+
+    public function changeLimit($limit)
+    {
+        $this->limit = in_array($limit, $this->limitList) ? $limit : $this->limitList[0];
+        $this->resetPage();
+    }
+
 
     public function delete(int $id)
     {
@@ -27,7 +46,7 @@ class UserList extends Component
     public function render($user = null)
     {
         return view('livewire.user.user-list', [
-            'users' => User::query()->with('country')->orderBy('id', 'desc')->paginate(10, pageName: 'users-page'),
+            'users' => User::query()->with('country')->orderBy('id', 'desc')->paginate($this->limit),
 //            'users' => User::query()->orderBy('id', 'desc')->simplePaginate(10, pageName: 'users-page'),
         ]);
     }
--- resources/views/livewire/user/user-list.blade.php
+++ resources/views/livewire/user/user-list.blade.php
@@ -1,22 +1,62 @@
 <div class="col-12">
 
-    <ul id="users-list">
-        @forelse($users as $user)
-            <li wire:key="{{ $user->id }}">
-                {{ $user->name }} ({{ $user->email  }}) |
-                {{ $user->country->name }}|
-                <a href="#"
-                   wire:click.prevent="delete({{ $user->id }})"
-                   wire:confirm="Are you sure?">Delete</a>
-                @if ($user->avatar)
-                    <img src="{{ asset("uploads/{$user->avatar}") }}" alt="" height="30">
-                @endif
-            </li>
-        @empty
-            <p>User list is empty...</p>
-        @endforelse
-    </ul>
+    <div class="d-flex justify-content-between mb-2" id="users-list">
+        <div>
+            {{--<select class="form-select" wire:model.live="limit">
+                @foreach($limitList as $k => $v)
+                    <option @if($v == $limit) selected @endif wire:key="{{ $k }}" value="{{ $v }}">{{ $v }}</option>
+                @endforeach
+            </select>--}}
+            <select class="form-select">
+                @foreach($limitList as $k => $v)
+                    <option @if($v == $limit) selected @endif wire:key="{{ $k }}" value="{{ $v }}" wire:click="changeLimit({{ $v }})">{{ $v }}</option>
+                @endforeach
+            </select>
+        </div>
 
-    {{ $users->links('vendor.livewire.my-bootstrap', data:['scrollTo' => '#users-list']) }}
+        <div>
+            <input type="text" class="form-control" id="search" placeholder="Search...">
+        </div>
+    </div>
+
+    <div class="table-responsive position-relative">
+
+        <div wire:loading style="position: absolute; width: 100%; height: 100%; background: rgba(255, 255, 255, .7); text-align: center; padding-top: 20px;">
+            <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+
+
+        <table class="table table-striped table-hover">
+
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Country</th>
+                <th>Delete</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            @foreach($users as $user)
+                <tr wire:key="{{ $user->id }}">
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->country->name }}</td>
+                    <td>
+                        <button wire:click="delete({{ $user->id }})" wire:confirm="Are you sure?" class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+
+        </table>
+    </div>
+
+    {{ $users->links(data:['scrollTo' => '#users-list']) }}
 
 </div>
